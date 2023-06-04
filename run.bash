3b560286f2520f27684503cc14f7a937edca204f
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2023-06-03 19:01:41 -0700

    add bash script for batching submit.bash script

--- run.bash
+++ run.bash
@@ -1,14 +1,3 @@
-#!/bin/bash
-#SBATCH -J mpitest
-#SBATCH -A cs475-575
-#SBATCH -p classmpitest
-#SBATCH -N 4 # number of nodes
-#SBATCH -n 4 # number of tasks
-#SBATCH --constraint=ib
-#SBATCH --tasks-per-node=1
-#SBATCH -o mpitest.out
-#SBATCH -e mpitest.err
 module  load  slurm
 module  load  openmpi
-mpic++ main.cpp -o fourier  -lm
-mpiexec -np 4 fourier
+sbatch submit.bash
